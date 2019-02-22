feb1c54b4b49f3955b3de47bce8a12801034f98f
Jacob Bryan <jtb36688@gmail.com>
2019-02-22 01:04:30 -0500

    Appropriately Made recipe_ingredients table2

--- seeds/004-RecipeIngredients.js
+++ seeds/004-RecipeIngredients.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex("ingredients")
+  return knex("recipe_ingredients")
     .truncate()
     .then(function() {
       // Inserts seed entries
-      return knex("ingredients").insert([
+      return knex("recipe_ingredients").insert([
         { recipes_id: 1, ingredients_id: 1  },
         { recipes_id: 2, ingredients_id: 2  },
         { recipes_id: 1, ingredients_id: 3  },
